845172ef7426adbcc7a9cc129482584fd108dd69
shreytailor17 <stai259@aucklanduni.ac.nz>
2020-09-20 01:08:32 +1200

    added settings bututon and shortcuts.

--- src/a3/quinzical/frontend/controllers/MainMenuController.java
+++ src/a3/quinzical/frontend/controllers/MainMenuController.java
@@ -2,13 +2,41 @@ package a3.quinzical.frontend.controllers;
 
 // JavaFX dependencies.
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
 
 public class MainMenuController {
+
+    @FXML
+    Label title;
+
+    @FXML
+    Button practiceModuleButton;
+
+    @FXML
+    Button gameModuleButton;
+
+    @FXML
+    Button exitButton;
 
     @FXML
     private void handleExitButton() {
         ScreenController screenController = ScreenController.getInstance();
         screenController.exit();
+    }
+
+    @FXML
+    private void handleKeyPressed(KeyEvent event) {
+        switch(event.getCode()) {
+            case Q:
+                exitButton.fireEvent(new ActionEvent());
+        }
+
+        /* --------------------------------------------------------
+        ADD MORE KEY SHORTCUTS HERE AS THE BUTTON FUNCTIONALITY IS ADDED.
+         -------------------------------------------------------- */
     }
 
 }
