0e7c70aeeda5e821d56fad90c4579778529208f2
shreytailor17 <stai259@aucklanduni.ac.nz>
2020-09-24 14:47:45 +1200

    switched to festival.

--- src/a3/quinzical/backend/Speaker.java
+++ src/a3/quinzical/backend/Speaker.java
@@ -4,6 +4,7 @@ package a3.quinzical.backend;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Speaker {
     private static Speaker _speaker;
@@ -13,6 +14,7 @@ public class Speaker {
     private String _speechString;
     private boolean _isChanged = false;
 
+    private long _pid;
     private Process _process;
     private ProcessBuilder _processBuilder;
 
@@ -60,25 +62,30 @@ public class Speaker {
     public void speak() {
         kill();
 
-        List<String> arguments = new ArrayList<String>();
-        arguments.add("espeak");
-        arguments.add(_speechString);
-        if (_isChanged) {
-            arguments.add("-s");
-            arguments.add(String.valueOf(SPEED));
-        }
-
         try {
-            _processBuilder = new ProcessBuilder(arguments);
+            _processBuilder = new ProcessBuilder("/bin/bash", "-c", "echo " + _speechString + " | festival --tts");
             _process = _processBuilder.start();
+            _pid = _process.pid();
         } catch (IOException error) {
-            // Due to implementation, this will never be thrown.
-        }
+            System.out.println(error);
+        };
     }
 
     public void kill() {
-        if (_process != null) {
-            _process.destroy();
-        }
+        /*
+         * This commented code could be used for killing all the descendents of the main process,
+         * however it is a little haphazard to do this, because it could be unsafe for the game.
+         * Stream<ProcessHandle> descendents = ProcessHandle.current().descendants();
+         *
+         * Hence a better approach was opted for by only destroying the descendents of the known
+         * process that is current speaking. Furthermore, it is enclosed in a try/catch block as
+         * when there is no process running and you try to destroy it, it will throw exception.
+         */
+        try {
+            Stream<ProcessHandle> descendents = _process.descendants();
+            descendents.filter(ProcessHandle::isAlive).forEach(ph -> {
+                ph.destroy();
+            });
+        } catch (Exception error) {  };
     }
 }
